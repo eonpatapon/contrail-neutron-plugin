eac0ff473962eae5ccbeea4df65eb489787a4d13
Jean-Philippe Braun <jean-philippe.braun@cloudwatt.com>
2015-12-15 18:10:47 +0100

    Use PEP0440 compliant version based on build date
    Change-Id: I843ebffb749b761a77c511c8cb5376e3eb2701b7

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 #
 
 from setuptools import setup, find_packages
+from datetime import datetime
+
 
 def requirements(filename):
     with open(filename) as f:
@@ -11,7 +13,7 @@ def requirements(filename):
 
 setup(
     name='neutron_plugin_contrail',
-    version='0.1dev',
+    version=datetime.now().strftime('%Y.%m+%d%H%M%S.cw'),
     packages=find_packages(),
     package_data={'': ['*.html', '*.css', '*.xml']},
     zip_safe=False,
@@ -21,4 +23,8 @@ setup(
 
     test_suite='neutron_plugin_contrail.tests',
     tests_require=requirements('test-requirements.txt'),
+
+    url='https://git.corp.cloudwatt.com/applications/contrail-neutron-plugin',
+    author='Jean-Philippe Braun',
+    author_email='jean-philippe.braun@cloudwatt.com'
 )
