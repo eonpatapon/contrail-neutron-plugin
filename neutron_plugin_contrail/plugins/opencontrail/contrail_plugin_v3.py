bb106e820dc9b889b66b678f2b178d823ed6be79
Antoine Eiche <antoine.eiche@cloudwatt.com>
2016-03-19 19:59:18 +0100

    Use two vnc_api connections to manage user roles
    Before doing a vnc_api call, the plugin sets the user role in the http
    header, according to the user who does the request. The user role is
    set in a common vnc_lib variable. However, the neutron request can
    contain several vnc_api calls. The user role of a request can then be
    overwritten by another request.
    
    Two vnc_lib objects are created, one for users and another one for
    admins, in order to avoid permission leaks.
    
    Change-Id: I53c6c0de70ac85199f2b11cc4d9a97b67167170f
    Partial-bug: #1559532

--- neutron_plugin_contrail/plugins/opencontrail/contrail_plugin_v3.py
+++ neutron_plugin_contrail/plugins/opencontrail/contrail_plugin_v3.py
@@ -110,6 +110,12 @@ class NeutronPluginContrailCoreV3(plugin_base.NeutronPluginContrailCoreBase):
                     auth_host=auth_host, auth_port=auth_port,
                     auth_protocol=auth_protocol, auth_url=auth_url,
                     auth_type=auth_type)
+                self._vnc_lib_member = vnc_api.VncApi(
+                    admin_user, admin_password, admin_tenant_name,
+                    api_srvr_ip, api_srvr_port, api_server_url,
+                    auth_host=auth_host, auth_port=auth_port,
+                    auth_protocol=auth_protocol, auth_url=auth_url,
+                    auth_type=auth_type)
                 connected = True
             except requests.exceptions.RequestException:
                 time.sleep(3)
@@ -127,7 +133,10 @@ class NeutronPluginContrailCoreV3(plugin_base.NeutronPluginContrailCoreBase):
         # token saved earlier in the pipeline
         if hasattr(self._vnc_lib, "set_auth_token"):
             self._vnc_lib.set_auth_token(context.auth_token)
-        self._vnc_lib.set_user_roles(context.roles)
+        if self.get_role_kind(context) == 'admin':
+            self._vnc_lib.set_user_roles(context.roles)
+        else:
+            self._vnc_lib_member.set_user_roles(context.roles)
 
     def _prepare_res_handlers(self):
         contrail_extension_enabled = cfg.CONF.APISERVER.contrail_extensions
@@ -135,32 +144,41 @@ class NeutronPluginContrailCoreV3(plugin_base.NeutronPluginContrailCoreBase):
         kwargs = {'contrail_extensions_enabled': contrail_extension_enabled,
                   'apply_subnet_host_routes': apply_subnet_host_routes}
 
-        self._res_handlers['network'] = vn_handler.VNetworkHandler(
-            self._vnc_lib, **kwargs)
+        self._res_handlers['network'] = self.vnc_per_role(
+            vn_handler.VNetworkHandler, **kwargs)
+        self._res_handlers['subnet'] = self.vnc_per_role(
+            subnet_handler.SubnetHandler, **kwargs)
+        self._res_handlers['port'] = self.vnc_per_role(
+            vmi_handler.VMInterfaceHandler, **kwargs)
+        self._res_handlers['router'] = self.vnc_per_role(
+            rtr_handler.LogicalRouterHandler, **kwargs)
+        self._res_handlers['floatingip'] = self.vnc_per_role(
+            fip_handler.FloatingIpHandler,  **kwargs)
+        self._res_handlers['security_group'] = self.vnc_per_role(
+            sg_handler.SecurityGroupHandler, **kwargs)
+        self._res_handlers['security_group_rule'] = self.vnc_per_role(
+            sgrule_handler.SecurityGroupRuleHandler, **kwargs)
+        self._res_handlers['ipam'] = self.vnc_per_role(
+            ipam_handler.IPamHandler, **kwargs)
+        self._res_handlers['policy'] = self.vnc_per_role(
+            policy_handler.PolicyHandler, **kwargs)
+        self._res_handlers['route_table'] = self.vnc_per_role(
+            route_table_handler.RouteTableHandler, **kwargs)
+        self._res_handlers['svc'] = self.vnc_per_role(
+            svc_instance_handler.SvcInstanceHandler, **kwargs)
+        self._res_handlers['virtual_router'] = self.vnc_per_role(
+            vrouter_handler.VirtualRouterHandler, **kwargs)
 
-        self._res_handlers['subnet'] = subnet_handler.SubnetHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['port'] = vmi_handler.VMInterfaceHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['router'] = rtr_handler.LogicalRouterHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['floatingip'] = fip_handler.FloatingIpHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['security_group'] = sg_handler.SecurityGroupHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['security_group_rule'] = (
-            sgrule_handler.SecurityGroupRuleHandler(self._vnc_lib, **kwargs))
+    def get_role_kind(self, context):
+        if 'admin' in context.roles:
+            return 'admin'
+        else:
+            return 'member'
 
-        self._res_handlers['ipam'] = ipam_handler.IPamHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['policy'] = policy_handler.PolicyHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['route_table'] = (
-            route_table_handler.RouteTableHandler(self._vnc_lib, **kwargs))
-        self._res_handlers['svc'] = svc_instance_handler.SvcInstanceHandler(
-            self._vnc_lib, **kwargs)
-        self._res_handlers['virtual_router'] = \
-            vrouter_handler.VirtualRouterHandler(self._vnc_lib, **kwargs)
+    def vnc_per_role(self, handler, **kwargs):
+        return {
+            'admin': handler(self._vnc_lib, **kwargs),
+            'member': handler(self._vnc_lib_member, **kwargs)}
 
     def _get_context_dict(self, context):
         return dict(context.__dict__)
@@ -171,32 +189,32 @@ class NeutronPluginContrailCoreV3(plugin_base.NeutronPluginContrailCoreBase):
                 del res_data[res_type][key]
 
         self._set_user_infos(context)
-        return self._res_handlers[res_type].resource_create(
+        return self._res_handlers[res_type][self.get_role_kind(context)].resource_create(
             self._get_context_dict(context), res_data[res_type])
 
     def _get_resource(self, res_type, context, id, fields):
         self._set_user_infos(context)
-        return self._res_handlers[res_type].resource_get(
+        return self._res_handlers[res_type][self.get_role_kind(context)].resource_get(
             self._get_context_dict(context), id, fields)
 
     def _update_resource(self, res_type, context, id, res_data):
         self._set_user_infos(context)
-        return self._res_handlers[res_type].resource_update(
+        return self._res_handlers[res_type][self.get_role_kind(context)].resource_update(
             self._get_context_dict(context), id, res_data[res_type])
 
     def _delete_resource(self, res_type, context, id):
         self._set_user_infos(context)
-        return self._res_handlers[res_type].resource_delete(
+        return self._res_handlers[res_type][self.get_role_kind(context)].resource_delete(
             self._get_context_dict(context), id)
 
     def _list_resource(self, res_type, context, filters, fields):
         self._set_user_infos(context)
-        return self._res_handlers[res_type].resource_list(
+        return self._res_handlers[res_type][self.get_role_kind(context)].resource_list(
             self._get_context_dict(context), filters, fields)
 
     def _count_resource(self, res_type, context, filters):
         self._set_user_infos(context)
-        res_count = self._res_handlers[res_type].resource_count(
+        res_count = self._res_handlers[res_type][self.get_role_kind(context)].resource_count(
             self._get_context_dict(context), filters)
         return {'count': res_count}
 
@@ -216,9 +234,9 @@ class NeutronPluginContrailCoreV3(plugin_base.NeutronPluginContrailCoreBase):
         port_id = interface_info.get('port_id')
         subnet_id = interface_info.get('subnet_id')
 
-        rtr_iface_handler = rtr_handler.LogicalRouterInterfaceHandler(
-            self._vnc_lib)
-        return rtr_iface_handler.add_router_interface(
+        rtr_iface_handler = self.vnc_per_role(
+            rtr_handler.LogicalRouterInterfaceHandler)
+        return rtr_iface_handler[self.get_role_kind(context)].add_router_interface(
             self._get_context_dict(context), router_id,
             port_id=port_id, subnet_id=subnet_id)
 
@@ -233,8 +251,8 @@ class NeutronPluginContrailCoreV3(plugin_base.NeutronPluginContrailCoreBase):
         subnet_id = interface_info.get('subnet_id')
 
         self._set_user_infos(context)
-        rtr_iface_handler = rtr_handler.LogicalRouterInterfaceHandler(
-            self._vnc_lib)
-        return rtr_iface_handler.remove_router_interface(
+        rtr_iface_handler = self.vnc_per_role(
+            rtr_handler.LogicalRouterInterfaceHandler)
+        return rtr_iface_handler[self.get_role_kind(context)].remove_router_interface(
             self._get_context_dict(context), router_id, port_id=port_id,
             subnet_id=subnet_id)
